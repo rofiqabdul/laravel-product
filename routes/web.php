a4730ea2213887a90a58ea2bc408884b77b9aaae
rofiqabdul <rofiqabdul04@gmail.com>
2024-11-26 18:28:55 +0700

    [3.Strukur Folder] - new route, add file css js, use simple blade engine

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Abdul Rofiq']);
+});
+
+// TUGAS: Buat 2 rute baru
+// 1. /blog -> 2 buah artikel, judul dan isi
+// 2. /contact -> email / social media
